9d2e5858e72eeb43b244c69da37a1b0e6f60f629
shubavelai <75782686+shubavelai@users.noreply.github.com>
2022-01-31 10:50:32 +0530

    Create db_bkup.php

---
+++ db_bkup.php
@@ -0,0 +1,31 @@
+<?php
+    $conn = new mysqli('localhost','root','','database_name');
+    $data = '';
+    $result = mysqli_query($conn,"SHOW TABLES");
+    while($row = mysqli_fetch_row($result))
+        $tables[] =  ($row[0]);
+    foreach($tables as $table){
+        $select = "SELECT * FROM `$table`";
+        $result = $conn->query($select);
+        if ($result->num_rows > 0)
+        {
+            while($row = $result->fetch_assoc())
+            {
+                $data .= json_encode(base64_encode(serialize($row)))."<br>";
+            }
+        }
+    }
+
+// Save the SQL script to a backup file
+    $backup_file_name = 'iBank_backup_' .date('dmY'). '.bkup';
+    $fileHandler = fopen($backup_file_name, 'w+');
+    $number_of_lines = fwrite($fileHandler, $data);
+    fclose($fileHandler);
+
+// Move saved back file into folder
+    $path = "bkups/".$backup_file_name;
+    if (copy($backup_file_name,$path)) 
+    {
+      unlink($backup_file_name);
+    }
+?>
